5fabcaefdb0048d3399c9923597229ce1a0bd8a7
Christina Kokkinaki <79784004+ChristinaKok@users.noreply.github.com>
2024-01-09 12:47:15 +0200

    Update clusters.h

--- clusters.h
+++ clusters.h
@@ -13,16 +13,22 @@ class Cluster{
 
 static vector<pair<int,double>> near2_cent;
 
-void k_means_pp(vector<vector<double>> ar, vector<vector<double>>&centroids,int k);
+void k_means_pp(const vector<vector<double>> &ar, vector<vector<double>> &centroids,int k);
 
-vector<Cluster> lloyds(int k, vector<vector<double>> centroids ,vector<vector<double>> ar);
+vector<Cluster> lloyds(int k, vector<vector<double>> &centroids ,const vector<vector<double>> &ar);
 
-void macqueen(vector<Cluster> &clusters,vector<vector<double>> ar,int p_index,int old_index,int new_index);
+void macqueen(vector<Cluster> &clusters,const vector<vector<double>> &ar,int p_index,int old_index,int new_index);
 
-void macqueen_all(vector<Cluster> &clusters, vector<vector<double>>& new_centroids, vector<vector<double>> ar);
+void macqueen_all(vector<Cluster> &clusters, vector<vector<double>>& new_centroids, const vector<vector<double>> &ar);
 
-vector<Cluster> rev_lsh(int L,int k_h,vector<vector<double>> ar,vector<vector<double>> centroids);
+vector<Cluster> rev_lsh(int L,int k_h,const vector<vector<double>> &ar,vector<vector<double>> &centroids);
 
-vector<Cluster> rev_cube(int k_h,vector<vector<double>> ar,vector<vector<double>> centroids,int M,int probes);
+vector<Cluster> rev_cube(int k_h,const vector<vector<double>> &ar,vector<vector<double>> &centroids,int M,int probes);
 
-vector<double> silhouette(vector<vector<double>> ar, vector<Cluster> clusters, string method);
+vector<double> silhouette(const vector<vector<double>> &ar, vector<Cluster> &clusters, string method);
+
+vector<double> silhouette_red(const vector<vector<double>> &ar, vector<Cluster> &clusters, string method, const vector<vector<double>> &new_ar);
+
+void convert(vector<Cluster> &clusters, const vector<vector<double>>& new_ar, const vector<vector<double>> &ar);
+
+double objective_function(const vector<vector<double>> &ar, const vector<Cluster> &clusters);
